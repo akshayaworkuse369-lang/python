acb58b75dfdf184522dd8979c67883e41501391a
unknown <akshayaworkuse369@gmail.com>
2026-04-14 13:03:39 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,32 @@
+# list
+a="Akshaya"
+print(a)
+b=1
+print(b)
+float=12.2
+print(float)
+c=[1,2,3]
+print(c[0])
+print(c[2])
+print(c[-1])
+c.append(10)
+print("After append(10):", c)
+c.insert(1,2)
+print (c)
+c.pop()  
+print(c)
+
+
+
+# tuple
+tup=("apple","banana","orange","cherry")
+print(tup)
+l = list(tup)
+print(l)
+l.append("grapes")
+print(l)
+tup=tuple(l)
+print(tup)
+
+
+
